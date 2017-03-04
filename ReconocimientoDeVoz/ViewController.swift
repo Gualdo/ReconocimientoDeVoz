d02b0b5e4dcf560d2c40926e93d4f4bb234ea4d0
Eduardo D De La Cruz Marr <gualdodelacruz@gmail.com>
2017-03-03 22:09:32 -0400

    Empezando la app

---
+++ ReconocimientoDeVoz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ReconocimientoDeVoz
+//
+//  Created by Eduardo D De La Cruz Marr on 3/3/17.
+//  Copyright © 2017 Eduardo D De La Cruz Marrero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
